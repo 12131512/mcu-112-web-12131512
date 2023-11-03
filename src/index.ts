8efe3e7eb4d97126b29f42ae82b5c24c62e1a032
12131512 <12131512@me.mcu.edu.tw>
2023-11-03 16:40:14 +0800

    feat(ts): 利用布林變數記錄待辦事項是否完成

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ let todoId: number;
 todoId = 1;
 console.log("Todo ID: ", todoId);
 
-
 const todo = "期中考作業 ";
 console.log("Todo Name: ", todo);
 
+const hasFinished = false;
+console.log("Has Finished:", hasFinished);
