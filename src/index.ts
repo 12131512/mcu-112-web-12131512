7f6989cacf56209d4f89565ef779028f9a68d46d
12131512 <12131512@me.mcu.edu.tw>
2023-11-03 18:15:31 +0800

    feat(ts): 利用陣列記錄多筆待辦事項

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-//const todo = {
-    //id:1,
-   // content:'期中考作業',
-    //hasFinished:false,
-    //finishDate:undefined
-//};
-console.log("To Do:",todo)
-console.table(todo);
+import { Todo } from "./todo";
+
+const todos = [
+    new Todo(1, "期中考作業"),
+    new Todo(2, "期中考作業-2"),
+    new Todo(3, "期中考作業-3"),
+  ];
+  
+  console.log("To Do: ", todos);
+  console.table(todos);
 
