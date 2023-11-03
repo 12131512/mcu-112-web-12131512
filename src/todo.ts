d16130014a741ed5f88ed406a297ac7a533f0463
12131512 <12131512@me.mcu.edu.tw>
2023-11-03 18:12:45 +0800

    refactor(ts): 變更待辦事項建構式

--- src/todo.ts
+++ src/todo.ts
@@ -1,14 +1,10 @@
- class Todo {
-    constructor(id:number,content:string){
-        this.id = id;
-        this.content = content;
-        this.hasFinished = false;
-    }
-
-    id:number;
-    content: string;
-    hasFinished:boolean;
-    finishDate:Date|undefined;
-}
-
-export const todo = new Todo(1, '期中考作業');
+export class Todo {
+    constructor(
+        public id: number,
+        public content: string,
+        public hasFinished = false
+      ) {}
+  
+      finishDate?: Date;
+  }
+  
